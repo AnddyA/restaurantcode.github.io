e381d20c8636162797bd12dfc7e238ed1c16a9fa
NotAnddy <118786591+NotAnddy@users.noreply.github.com>
2023-01-16 20:04:03 -0500

    Add files via upload

---
+++ RestauranteApp/src/rest/restaurante/RESTAURANTE.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rest.restaurante;
+
+import rest.panels.PanelCarga;
+
+/**
+ *
+ * @author HP
+ */
+public class RESTAURANTE {
+    
+    
+    
+    
+    public static void main(String[] args) {
+
+        PanelCarga p1 = new PanelCarga();
+        p1.setVisible(true);
+
+    }
+}
